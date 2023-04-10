d7df19f7a1da9020f9455b925b806d349a6d3b56
tilmanmixyz <tilmanmixyz@proton.me>
2023-04-10 17:18:06 +0200

    modifying nix flake to actually work with graphics

--- flake.nix
+++ flake.nix
@@ -1,38 +1,62 @@
 {
   inputs = {
+    naersk.url = "github:nix-community/naersk";
+    utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
-  outputs = {self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-      {
-        packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
-          pname = "readlines";
-          version = "0.0.1";
-          src = ./.;
-          cargoLock = {
-            lockFile = ./Cargo.lock;
+  outputs = {self, nixpkgs, naersk, utils, ... }:
+    utils.lib.eachDefaultSystem (system: 
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
+        libPath = with pkgs; lib.makeLibraryPath [
+          libGL
+          libxkbcommon
+          wayland
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr
+        ];
+      in
+        {
+          defaultPackage = naersk-lib.buildPackage {
+            src = ./.;
+            doCheck = true;
+            pname = "readlines";
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+              makeWrapper
+            ];
+            buildInputs = with pkgs; [
+              xorg.libxcb
+            ];
+            postInstall = ''
+            wrapProgram "$out/bin/readlines" --prefix LD_LIBRARY_PATH : "${libPath}"
+          '';
           };
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-          ];
-          buildInputs = with pkgs; [
-            openssl
-          ];
-        };
-        devShells.${system}.default = pkgs.mkShell {
-          packages = with pkgs; [
-            bacon
-          ];
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-          ];
-          buildInputs = with pkgs; [
-            cargo
-            openssl
-          ];
-        };
-      };
+          defaultApp = utils.lib.mkApp {
+            drv = self.defaultPackage.${system};
+          };
+          devShell = with pkgs; mkShell {
+            nativeBuildInputs = [
+              pkg-config
+            ];
+            buildInputs = [
+              cargo
+              rustc
+              rustPackages.clippy
+              rustfmt
+              tokei
+
+              openssl
+              xorg.libxcb
+            ];
+            LD_LIBRARY_PATH = libPath;
+          };
+        });
 }
